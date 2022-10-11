4df3cb9d6ba9f567876dbf523d96cd25aa9d3b65
Siddhesh Pardeshi <78296149+sidx2@users.noreply.github.com>
2022-10-11 14:46:13 +0530

    formatting and dp

--- fib.cpp
+++ fib.cpp
@@ -1,37 +1,19 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int fib(int n)
+int dp[1e4] = {0};
 
-{
-
-    if(n==0)
-
-    return 0;
-
-    else if(n==1)
-
-    return 1;
-
-    else
-
-    return fib(n-1)+fib(n-2);
-
+int fib(int n) {
+    if(n==0||n==1)return n;
+    if(dp[n])return dp[n];
+    dp[n] = fib(n-1)+fib(n-2);
+    return dp[n];
 }
 
-int main()
-
-{
-
+int main() {
     int n
-
     cout<<"Enter n to find nth number in Fibonacci Sequence : "
-
     cin>>n
-
     cout<<"The nth integer in Fibonacci Sequence = "
-
     cout<<fib(n)<<endl
-
 }
