d486b5f250e9914b5170051ad8ec99bb80d54f83
Siddhesh Pardeshi <78296149+sidx2@users.noreply.github.com>
2022-10-20 15:39:06 +0530

    Create divisibleBy2.cpp
    plz complete the function

---
+++ divisibleBy2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isDivisibleBy2(int n) {
+  ; // Complete this function
+}
+
+int main() {
+  if(isDivisibleBy2(38)) cout << "YES\n";
+  else cout << "NO\n";
+}
